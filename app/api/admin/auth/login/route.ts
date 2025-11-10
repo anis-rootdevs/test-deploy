5b64d44483cf422043ab8e92cfbca2f7e9320c66
Muhammad Anisuzzaman <anisuzzaman.rootdevs@gmail.com>
2025-11-10 15:52:43 +0600

    refactor: async handler

--- app/api/admin/auth/login/route.ts
+++ app/api/admin/auth/login/route.ts
@@ -1,24 +1,14 @@
-import { apiResponse } from "@/lib/api-response";
 import { asyncHandler } from "@/lib/async-handler";
-import { validateRequest } from "@/lib/validate-request";
+import { apiResponse } from "@/lib/utils";
 import { loginSchema } from "@/lib/validation-schema";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import z from "zod";
 
-export const POST = asyncHandler(async (req: NextRequest) => {
-  const { data, error } = await validateRequest(req, loginSchema);
+export const POST = asyncHandler(
+  loginSchema,
+  async (req: NextRequest, data: z.infer<typeof loginSchema>) => {
+    console.log({ data });
 
-  if (error) {
-    return NextResponse.json(
-      {
-        status: error.status,
-        error: error.message,
-        details: error.details,
-      },
-      { status: error.statusCode }
-    );
+    return apiResponse(true, 200, "Login successfully!");
   }
-
-  console.log({ data });
-
-  return apiResponse(true, 200, "Login successfully!");
-});
+);
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { NextResponse } from "next/server";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function apiResponse<T = undefined>(
+  status: boolean,
+  statusCode: number = 200,
+  message: string,
+  data?: T,
+  pagination?: T
+) {
+  return NextResponse.json(
+    {
+      status,
+      message,
+      data,
+      pagination,
+    },
+    { status: statusCode }
+  );
 }
