59dd1102b437c45538160062155cdc94730c3d90
Mahmudtareq <tareqlpu5@gmail.com>
2025-12-01 11:20:16 +0600

    refactor: products search and filters

--- app/(private)/admin/dashboard/products/_components/ProductTableToolbar.tsx
+++ app/(private)/admin/dashboard/products/_components/ProductTableToolbar.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { DynamicBreadcrumb } from "@/components/custom/DynamicBreadcrumb";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { routes } from "@/config/routes";
-import { Category } from "@/lib/types";
-import { Plus } from "lucide-react";
+import { useTableState } from "@/store/useTableStore";
+import { Plus, X } from "lucide-react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { useTransition } from "react";
+import ProductFilter from "./ProductFilter";
+import SearchBar from "./SearchBar";
 
 interface ProductsTableToolbarProps<TData> {
-  categories: Category[];
   tableId: string;
 }
 
@@ -21,55 +21,114 @@ const breadcrumbItems = [
 ];
 
 export default function ProductTableToolbar<TData>({
-  categories,
   tableId,
 }: ProductsTableToolbarProps<TData>) {
-  const router = useRouter();
-  const pathname = usePathname();
-  const [isPending, startTransition] = useTransition();
+  const {
+    search,
+    searchInput,
+    filters,
+    localState,
+    setSearch,
+    setSearchInput,
+    setFilter,
+    setLocalState,
+  } = useTableState(tableId);
 
+  const handleClearFilters = () => {
+    setSearch("");
+    setFilter("type", "all");
+  };
+
+  const activeFilters = [];
+
+  if (search) {
+    activeFilters.push({
+      label: `Search: ${search}`,
+      key: "search",
+      onRemove: () => setSearch(""),
+    });
+  }
+  if (filters.type && filters.type !== "all") {
+    activeFilters.push({
+      label: `Type: ${localState.type}`,
+      key: "status",
+      onRemove: () => {
+        setFilter("type", "all");
+        setLocalState("type", "");
+      },
+    });
+  }
+
+  const hasActiveFilters = activeFilters.length > 0;
   return (
     <div>
       {" "}
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col flex-1 gap-2">
-          <h2 className="font-jost font-medium text-lg">Manage Products</h2>
-          <DynamicBreadcrumb items={breadcrumbItems} />
-          <div className="flex items-center gap-4">
-            {/* <Input
-              placeholder="Search products..."
-              value={
-                (table.getColumn("name")?.getFilterValue() as string) ?? ""
-              }
-              onChange={(event) =>
-                table.getColumn("name")?.setFilterValue(event.target.value)
-              }
-              className="h-10 w-[150px] lg:w-[250px] rounded-[4px]"
-              disabled={isLoading || isPending}
-            />
-            <ProductFilter
-              filter={filter}
-              setFilter={setFilter}
-              disabled={isLoading || isPending}
-            /> */}
+      <div className="flex flex-col space-y-2">
+        <div className="flex items-start flex-wrap justify-between gap-2">
+          <div>
+            <h2 className="font-jost font-medium text-lg">Manage Products</h2>
+            <DynamicBreadcrumb items={breadcrumbItems} />
+          </div>
+
+          <div>
+            <Link href={routes.privateRoutes.admin.products.create}>
+              <Button
+                size="sm"
+                className="font-jost font-medium rounded-sm h-9 cursor-pointer"
+              >
+                <Plus className="h-4 w-4" />
+                Add New Product
+              </Button>
+            </Link>
           </div>
         </div>
-        <div className="flex items-center gap-2">
-          {/* <ProductsFormModal
-            isEditMode={false}
-            categories={categories}
-            onSuccess={handleRefresh}
-          /> */}
+        <div className="flex flex-wrap items-center gap-2">
+          <SearchBar
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
 
-          <Link href={routes.privateRoutes.admin.products.create}>
-            <Button
-              size="sm"
-              className="font-jost font-medium rounded-sm h-9 cursor-pointer"
-            >
-              <Plus className="h-4 w-4" />
-              Add New Product
-            </Button>
-          </Link>
+          <ProductFilter
+            value={(filters.type as string) || "all"}
+            onChange={(value, label) => {
+              setFilter("type", value);
+              setLocalState("type", label);
+            }}
+          />
+          {/* Active Filters with Clear Button */}
+          {hasActiveFilters && (
+            <div className="flex items-center gap-2 mt-2 md:mt-0">
+              <span className="text-sm font-medium text-muted-foreground">
+                Active filter:
+              </span>
+              {activeFilters.map((filter) => (
+                <Badge
+                  key={filter.key}
+                  variant="secondary"
+                  className="gap-1 pr-1 text-xs capitalize"
+                >
+                  {filter.label}
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="size-4 hover:bg-transparent hover:text-background/70"
+                    onClick={filter.onRemove}
+                  >
+                    <X className="size-3" />
+                    <span className="sr-only">Remove filter</span>
+                  </Button>
+                </Badge>
+              ))}
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={handleClearFilters}
+                className="h-6 gap-1 text-xs"
+              >
+                Clear all
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- app/(private)/admin/dashboard/products/_components/ProductsList.tsx
+++ app/(private)/admin/dashboard/products/_components/ProductsList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Category, Products } from "@/lib/types";
+import { Products } from "@/lib/types";
 
 import { getProductList } from "@/actions/product/productActions";
 import { DataTableWithPagination } from "@/components/custom/data-table/DataTableWithPagination";
@@ -9,20 +9,31 @@ import { useEffect, useState } from "react";
 import { columns } from "./columns";
 import ProductTableToolbar from "./ProductTableToolbar";
 
-interface ProductsListProps {
-  categories: Category[];
-}
-
-export default function ProductsList({ categories }: ProductsListProps) {
+export default function ProductsList() {
   const tableId = "product";
   const [data, setData] = useState<Products[]>([]);
   const [total, setTotal] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
-  const { page, limit, refresh } = useTableState(tableId);
+  const { page, limit, search, filters } = useTableState(tableId);
 
-  const fetchProducts = async (page: number, limit: number) => {
+  const fetchProducts = async (
+    page: number,
+    limit: number,
+    search: string,
+    filters: Record<string, unknown>
+  ) => {
     try {
-      const result = await getProductList(page, limit);
+      const cleanedFilters = Object.fromEntries(
+        Object.entries(filters).filter(
+          ([key, value]) => !!value && value !== "all" && key !== "type"
+        )
+      );
+      const result = await getProductList(page, limit, search, {
+        ...cleanedFilters,
+        ...(filters.type && filters.type !== "all"
+          ? { tag: filters.type }
+          : {}),
+      });
       setData(result?.data?.docs);
       setTotal(result?.data.totalDocs);
     } catch (error) {
@@ -34,16 +45,16 @@ export default function ProductsList({ categories }: ProductsListProps) {
 
   useEffect(() => {
     setIsLoading(true);
-    fetchProducts(page, limit);
-  }, [page, limit]);
+    fetchProducts(page, limit, search, filters);
+  }, [page, limit, search, filters]);
 
   useEffect(() => {
-    fetchProducts(page, limit);
-  }, [refresh]);
+    fetchProducts(page, limit, search, filters);
+  }, [page, limit, search, filters]);
 
   return (
     <div className="flex flex-col gap-6">
-      <ProductTableToolbar categories={categories} tableId={tableId} />
+      <ProductTableToolbar tableId={tableId} />
 
       <DataTableWithPagination
         data={data}
