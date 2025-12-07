d42a797d5e35b36cc5a7069942f708cc5c571e6f
Mahmudtareq <tareqlpu5@gmail.com>
2025-12-07 15:28:08 +0600

    fix: build iddude

--- app/(public)/policy/page.tsx
+++ app/(public)/policy/page.tsx
@@ -1,5 +1,6 @@
 import { getAllSettingsDetails } from "@/actions/settings/settingsActions";
 import PrivacyPolicy from "./_components/PrivacyPolicy";
+export const dynamic = "force-dynamic";
 
 export default async function PrivacyPolicyPage() {
   const policy = await getAllSettingsDetails("termsPolicy");
--- app/(public)/terms/page.tsx
+++ app/(public)/terms/page.tsx
@@ -1,5 +1,6 @@
 import { getAllSettingsDetails } from "@/actions/settings/settingsActions";
 import TermsAndConditions from "./_components/TermsAndConditions";
+export const dynamic = "force-dynamic";
 
 export default async function TermsHome() {
   const terms = await getAllSettingsDetails("termsPolicy");
