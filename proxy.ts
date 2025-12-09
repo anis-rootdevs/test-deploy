5b144d4963c54d19bd78de69dbf66e2912aeee29
Mahmudtareq <tareqlpu5@gmail.com>
2025-12-09 17:21:13 +0600

    fix: dashboard redirect issues

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import { auth } from "./app/api/auth/[...nextauth]/auth";
 import { routes } from "./config/routes";
 import { extractRoutes } from "./lib/utils";
 
@@ -10,25 +10,43 @@ function isProtectedRoute(pathname: string): boolean {
   return adminRoutes.some((route) => pathname.startsWith(route));
 }
 
-export async function proxy(req: NextRequest) {
-  const token = await getToken({ req, secret });
+export default auth((req) => {
   const { pathname } = req.nextUrl;
+  const accessToken = req?.auth?.token;
+  const isProtected = isProtectedRoute(pathname);
 
-  // const isProtected = isProtectedRoute(pathname);
+  if (isProtected && !accessToken) {
+    const loginUrl = new URL(routes.publicRoutes.adminLogin, req.url);
+    return NextResponse.redirect(loginUrl);
+  }
 
-  // if (isProtected && !token) {
-  //   const loginUrl = new URL(routes.publicRoutes.adminLogin, req.url);
-  //   return NextResponse.redirect(loginUrl);
-  // }
-
-  // if (pathname === routes.publicRoutes.adminLogin && token) {
-  //   return NextResponse.redirect(
-  //     new URL(routes.privateRoutes.admin.dashboard, req.url)
-  //   );
-  // }
+  if (pathname === routes.publicRoutes.adminLogin && accessToken) {
+    return NextResponse.redirect(
+      new URL(routes.privateRoutes.admin.dashboard, req.url)
+    );
+  }
 
   return NextResponse.next();
-}
+});
+// export async function proxy(req: NextRequest) {
+//   const token = await getToken({ req, secret });
+//   const { pathname } = req.nextUrl;
+
+//   // const isProtected = isProtectedRoute(pathname);
+
+//   // if (isProtected && !token) {
+//   //   const loginUrl = new URL(routes.publicRoutes.adminLogin, req.url);
+//   //   return NextResponse.redirect(loginUrl);
+//   // }
+
+//   if (pathname === routes.publicRoutes.adminLogin && token) {
+//     return NextResponse.redirect(
+//       new URL(routes.privateRoutes.admin.dashboard, req.url)
+//     );
+//   }
+
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: ["/admin/:path*"],
